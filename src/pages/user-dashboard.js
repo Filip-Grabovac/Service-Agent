edb76d1b13d33c184bd107567eb33d0b8578b36e
Stefan <skuzmic25@gmail.com>
2024-12-18 11:36:45 +0100

    commit

--- src/pages/user-dashboard.js
+++ src/pages/user-dashboard.js
@@ -1,7 +1,7 @@
 // import User from '../User';
 // import { fillTable, updateActiveElement, updateActiveRole } from '../Table.js';
 import User from 'https://cdn.jsdelivr.net/gh/Filip-Grabovac/Service-Agent@c965cbcc5c8e92901f58a18d6a2114978a7361cb/src/User.js';
-import {fillTable, updateActiveElement, updateActiveRole} from 'https://cdn.jsdelivr.net/gh/Filip-Grabovac/Service-Agent@91c2a47f33e93f1cb91111854743ebb4c52aec69/src/pages/admin-dashboard.js';
+import {fillTable, updateActiveElement, updateActiveRole} from 'https://cdn.jsdelivr.net/gh/Filip-Grabovac/Service-Agent@c86faab2378b02d2b4a90017f086fcf3ae95cb0f/src/Table.js';
 
 const user = new User();
 
