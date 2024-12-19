905e0f5d313bc9be115aab452498e97527febe93
Stefan <skuzmic25@gmail.com>
2024-12-19 09:33:24 +0100

    commit

--- src/pages/user-dashboard.js
+++ src/pages/user-dashboard.js
@@ -30,3 +30,38 @@ userMenu2.addEventListener('click', function (event) {
 })
 
 userMenu1.click()
+
+
+const pdfUrl = 'https://xjwh-2u0a-wlxo.n7d.xano.io/vault/V6ZqH-Ao/aglusEDHreNj7x8x_JiZswm1Yxs/-25oQQ../pdf-sample_0.pdf';
+
+const canvas = document.getElementById('pdf-canvas');
+const context = canvas.getContext('2d');
+
+const loadingTask = pdfjsLib.getDocument(pdfUrl);
+
+loadingTask.promise.then((pdf) => {
+    console.log('PDF učitan:', pdf);
+
+    pdf.getPage(1).then((page) => {
+        console.log('Stranica učitana:', page);
+
+        const viewport = page.getViewport({ scale: 1.5 });
+        canvas.width = viewport.width;
+        canvas.height = viewport.height;
+
+        const renderContext = {
+            canvasContext: context,
+            viewport: viewport,
+        };
+
+        page.render(renderContext).promise.then(() => {
+            console.log('Renderovanje završeno');
+        }).catch((error) => {
+            console.error('Greška pri renderovanju stranice:', error);
+        });
+    }).catch((error) => {
+        console.error('Greška pri učitavanju stranice:', error);
+    });
+}).catch((error) => {
+    console.error('Greška pri učitavanju PDF-a:', error);
+});
