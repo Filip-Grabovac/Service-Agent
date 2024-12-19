73fb2a08e62eb6543c13cc4711ee184a1bfde765
Stefan <skuzmic25@gmail.com>
2024-12-19 11:22:57 +0100

    commit

--- src/Table.js
+++ src/Table.js
@@ -749,9 +749,7 @@ function setPdf(pdfUrl) {
                 const canvas = document.createElement('canvas');
                 const context = canvas.getContext('2d');
 
-                const containerWidth = pdfContainer.clientWidth;
-                const viewport = page.getViewport({ scale: containerWidth / page.getViewport({ scale: 1 }).width });
-
+                const viewport = page.getViewport({ scale: 1.5 });
                 canvas.width = viewport.width;
                 canvas.height = viewport.height;
 
