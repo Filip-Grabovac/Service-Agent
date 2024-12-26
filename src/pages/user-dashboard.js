9d50e14515f947ef77f5fa15b36b3086d9738c8f
Stefan <skuzmic25@gmail.com>
2024-12-26 12:54:45 +0100

    commit

--- src/pages/user-dashboard.js
+++ src/pages/user-dashboard.js
@@ -41,6 +41,12 @@ userMenu2.addEventListener('click', function (event) {
 })
 
 userMenu1.click()
+
+home.addEventListener('click', function (event) {
+    event.stopImmediatePropagation();
+    event.preventDefault();
+})
+
 user.me().then((data) => {
     Array.from(profileImages).forEach((element) => {
         element.setAttribute('data-modal-open', 'edit-user-popup');
@@ -51,35 +57,4 @@ user.me().then((data) => {
     gear.setAttribute('data-fill-auth-id', '1');
     gear.setAttribute('data-id-user-id', data.id);
     setModals('initial-user');
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Funkcija za deaktivaciju tabova
-    function disableTabs() {
-        // Selektuj tab linkove sa custom atributom
-        var disabledTabs = document.querySelectorAll('[data-disabled-tab="true"]');
-
-        // Dodaj event listener na svaki od njih
-        disabledTabs.forEach(function (tab) {
-            tab.addEventListener('click', function (event) {
-                event.stopImmediatePropagation(); // Sprečava Webflow event
-                event.preventDefault(); // Sprečava default ponašanje linka
-                console.log('Klik na deaktiviran tab link.');
-            });
-        });
-    }
-
-    // Pozovi funkciju odmah
-    disableTabs();
-
-    // Posmatrač promene DOM-a za dinamički sadržaj
-    var observer = new MutationObserver(function () {
-        disableTabs(); // Ponovo inicijalizuj funkciju na promenu DOM-a
-    });
-
-    // Posmatraj promene na celom dokumentu
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-    });
 });
