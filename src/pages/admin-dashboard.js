1017d55344ef74905e753c6d14f06a9c64506318
Stefan <skuzmic25@gmail.com>
2024-12-26 12:49:47 +0100

    commit

--- src/pages/admin-dashboard.js
+++ src/pages/admin-dashboard.js
@@ -182,3 +182,34 @@ function getTabCount() {
         return sum;
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Funkcija za deaktivaciju tabova
+    function disableTabs() {
+        // Selektuj tab linkove sa custom atributom
+        var disabledTabs = document.querySelectorAll('[data-disabled-tab="true"]');
+
+        // Dodaj event listener na svaki od njih
+        disabledTabs.forEach(function (tab) {
+            tab.addEventListener('click', function (event) {
+                event.stopImmediatePropagation(); // Sprečava Webflow event
+                event.preventDefault(); // Sprečava default ponašanje linka
+                console.log('Klik na deaktiviran tab link.');
+            });
+        });
+    }
+
+    // Pozovi funkciju odmah
+    disableTabs();
+
+    // Posmatrač promene DOM-a za dinamički sadržaj
+    var observer = new MutationObserver(function () {
+        disableTabs(); // Ponovo inicijalizuj funkciju na promenu DOM-a
+    });
+
+    // Posmatraj promene na celom dokumentu
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
+});
