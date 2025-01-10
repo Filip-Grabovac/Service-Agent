3f6061ec76ceb2da5e8d2c18ade4ccc14cabb0a9
Stefan <skuzmic25@gmail.com>
2025-01-10 09:57:06 +0100

    commit

--- src/Table.js
+++ src/Table.js
@@ -876,8 +876,7 @@ export function setModals(menu) {
                         usersTable.classList.remove("hide");
                     }
 
-                    console.log(menu)
-                    if (menu === 1 || menu === 3 || menu === 4) {
+                    if (menu === 4 || menu === 'initial-admin') {
                         populateSelectWithShippingTariffs()
                     }
                     if (menu === 2) {
