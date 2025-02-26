984b0ccb5d2f31d1461db52c9b47947aa3e28789
Stefan <skuzmic25@gmail.com>
2025-02-26 10:58:50 +0100

    commit

--- src/pages/register.js
+++ src/pages/register.js
@@ -43,7 +43,7 @@ const iti = window.intlTelInput(phoneInput, {
     utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/utils.js",
     formatOnDisplay: false,
     separateDialCode: true,
-    customPlaceholder: "",
+    autoPlaceholder: "off",
 });
 
 const today = new Date().toISOString().split('T')[0];
