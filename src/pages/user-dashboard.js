4bb3d1bdfa6a71ed45fa14c27b94da4ab601d0e7
Stefan <skuzmic25@gmail.com>
2025-02-27 13:53:12 +0100

    commit

--- src/pages/user-dashboard.js
+++ src/pages/user-dashboard.js
@@ -361,35 +361,37 @@ isMedical.addEventListener('click', function (event) {
     }
 })
 
-const billingIcons = document.querySelectorAll('[data-billing-open]')
+document.addEventListener("DOMContentLoaded", function() {
+    const billingIcons = document.querySelectorAll('[data-billing-open]')
 
-billingIcons.forEach(element => {
-    element.addEventListener('click', () => {
-        const certificateId = element.getAttribute('data-billing-open');
+    billingIcons.forEach(element => {
+        element.addEventListener('click', () => {
+            const certificateId = element.getAttribute('data-billing-open');
 
-        fetch('https://xjwh-2u0a-wlxo.n7d.xano.io/api:UQuTJ3vx/portal-sessions', {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${authToken}`,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                success_url: "https://agent-for-service-cbd62c.webflow.io/user-dashboard",
-                certificate_id: certificateId,
-            }),
-        })
-            .then((response) => response.json())
-            .then((result) => {
-                if (result.code) {
-                    this.showError('Server Error! Please, try again or contact support.');
-
-                    return;
-                }
-
-                window.location.href = result;
+            fetch('https://xjwh-2u0a-wlxo.n7d.xano.io/api:UQuTJ3vx/portal-sessions', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${authToken}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    success_url: "https://agent-for-service-cbd62c.webflow.io/user-dashboard",
+                    certificate_id: certificateId,
+                }),
             })
-            .catch((error) => {
-                this.showError('Server Error! Please, try again or contact support.');
-            });
+                .then((response) => response.json())
+                .then((result) => {
+                    if (result.code) {
+                        this.showError('Server Error! Please, try again or contact support.');
+
+                        return;
+                    }
+
+                    window.location.href = result;
+                })
+                .catch((error) => {
+                    this.showError('Server Error! Please, try again or contact support.');
+                });
+        });
     });
 });
