4bf7cb68db5d8ba8324153ac8c15330a3f9918a0
Stefan <skuzmic25@gmail.com>
2025-04-25 13:10:52 +0200

    commit

--- src/pages/forgot.js
+++ src/pages/forgot.js
@@ -1,58 +1,58 @@
-import User from '../User.js';
-
-const user = new User();
-
-const resetBtn = document.getElementById('reset');
-const emailInput = document.getElementById('email');
-
-const errorWrapper = document.getElementById('error-wrapper');
-const errorMessage = document.getElementById('error-message');
-const errorClose = document.getElementById('error-close');
-
-errorClose.addEventListener('click', (e) => {
-    errorWrapper.classList.add('hide');
-})
-
-user.authenticate();
-
-resetBtn.addEventListener('click', function (event) {
-    event.preventDefault(); // Prevent form from submitting
-
-    const data = {
-        email: emailInput.value,
-    };
-
-    if (validateData(data) === 1) {
-        errorMessage.innerHTML = 'Please, fill in all fields.';
-        errorWrapper.classList.remove('hide');
-
-        setTimeout(function() {
-            errorWrapper.classList.add('hide');
-        }, 3000);
-
-        return;
-    }
-
-    user.forgot(data);
-});
-
-function validateData(data) {
-    let hasErrors = 0;
-
-    Object.entries(data).forEach(([key, value]) => {
-        if (value.length === 0) {
-            hasErrors = 1;
-        }
-
-        if (key === 'email' && !isValidEmail(value)) {
-            hasErrors = 1;
-        }
-    });
-
-    return hasErrors;
-}
-
-function isValidEmail(email) {
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailPattern.test(email);
-}
+// import User from '../User.js';
+//
+// const user = new User();
+//
+// const resetBtn = document.getElementById('reset');
+// const emailInput = document.getElementById('email');
+//
+// const errorWrapper = document.getElementById('error-wrapper');
+// const errorMessage = document.getElementById('error-message');
+// const errorClose = document.getElementById('error-close');
+//
+// errorClose.addEventListener('click', (e) => {
+//     errorWrapper.classList.add('hide');
+// })
+//
+// user.authenticate();
+//
+// resetBtn.addEventListener('click', function (event) {
+//     event.preventDefault(); // Prevent form from submitting
+//
+//     const data = {
+//         email: emailInput.value,
+//     };
+//
+//     if (validateData(data) === 1) {
+//         errorMessage.innerHTML = 'Please, fill in all fields.';
+//         errorWrapper.classList.remove('hide');
+//
+//         setTimeout(function() {
+//             errorWrapper.classList.add('hide');
+//         }, 3000);
+//
+//         return;
+//     }
+//
+//     user.forgot(data);
+// });
+//
+// function validateData(data) {
+//     let hasErrors = 0;
+//
+//     Object.entries(data).forEach(([key, value]) => {
+//         if (value.length === 0) {
+//             hasErrors = 1;
+//         }
+//
+//         if (key === 'email' && !isValidEmail(value)) {
+//             hasErrors = 1;
+//         }
+//     });
+//
+//     return hasErrors;
+// }
+//
+// function isValidEmail(email) {
+//     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     return emailPattern.test(email);
+// }
