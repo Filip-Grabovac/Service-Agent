a99dc7098c93f133aabca6d0e9fd6dbb286835a9
kuzmic3 <skuzmic25@gmail.com>
2025-06-17 14:16:53 +0200

    commit

--- src/pages/register.js
+++ src/pages/register.js
@@ -140,12 +140,16 @@ nextBtn.addEventListener("click", function (event) {
   event.preventDefault(); // Prevent form from submitting
 
   const termsLabel = document.getElementById("terms-label");
-  const termsLabelLink = termsLabel.querySelector("a");
+  const termsLabelLinks = termsLabel.querySelectorAll("a");
   termsLabel.style.color = "#475467";
-  termsLabelLink.style.color = "#475467";
+  termsLabelLinks.forEach(link => {
+    link.style.color = "#475467";
+  });
   if (!terms.checked) {
     termsLabel.style.color = "#ce0003";
-    termsLabelLink.style.color = "#ce0003";
+    termsLabelLinks.forEach(link => {
+      link.style.color = "#ce0003";
+    });
     // errorMessage.innerHTML = 'You must agree to the Terms & Conditions.';
     // errorWrapper.classList.remove('hide');
     //
