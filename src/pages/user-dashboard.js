04f71c5429686fa826cb96ce355020217fc2dbf1
kuzmic3 <skuzmic25@gmail.com>
2025-07-24 13:21:26 +0200

    commit

--- src/pages/user-dashboard.js
+++ src/pages/user-dashboard.js
@@ -244,7 +244,7 @@ user.me().then((data) => {
 
             setTimeout(() => {
                 gear.click();
-            }, 200)
+            }, 100)
         });
 
         document.querySelector(('.referral-close-icon')).addEventListener('click', (e) => {
