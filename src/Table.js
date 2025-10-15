ec8004bcfbefa80d2c1f25cfcaf1096f06782701
Stefan Kuzmic <skuzmic25@gmail.com>
2025-10-15 10:34:00 +0200

    dynamic shipping prices

--- src/Table.js
+++ src/Table.js
@@ -2712,12 +2712,11 @@ function populateDeliveryAddress(address) {
     }
 
     if (address.updated) {
-        console.log(authUserData)
         let fullUserAddress = authUserData._user_addresses_of_user.street + ' ' + authUserData._user_addresses_of_user.number + ', ' + authUserData._user_addresses_of_user.zip + ' ' + authUserData._user_addresses_of_user.city + ' - ' + authUserData._user_addresses_of_user.country;
         if (authUserData._user_addresses_of_user.address_additional) {
             fullUserAddress = fullUserAddress + ', ' + authUserData._user_addresses_of_user.address_additional;
         }
-        addressSelect.add(new Option(fullUserAddress, 'user_address'), 1);
+        addressSelect.add(new Option(fullUserAddress, 'user_address'), 0);
     }
 
     let fullAddress = address.street + ' ' + address.number + ', ' + address.zip + ' ' + address.city + ' - ' + address.country;
